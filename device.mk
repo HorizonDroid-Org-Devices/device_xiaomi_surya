205e258bdb25e9e8a32af510673820e982fdabbf
HinohArata <mmgcntk@gmail.com>
2024-03-08 19:12:09 +0000

    surya: remove all configuration for 'Remove packages'

--- device.mk
+++ device.mk
@@ -396,10 +396,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
 
-# Remove packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.multihal
